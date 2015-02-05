8cb6e0b11dba251cfb643c3f021cda4c192e2469
Aliaksandr Kavaliou <Aliaksandr_Kavaliou@epam.com>
2015-02-05 16:32:14 +0300

    Connector rewrited as Demand Holder Singleton

--- src/by/epam/talab/task1/action/Connector.java
+++ src/by/epam/talab/task1/action/Connector.java
@@ -3,16 +3,17 @@ package by.epam.talab.task1.action;
 import it.sauronsoftware.ftp4j.FTPClient;
 
 public final class Connector {
-	private static FTPClient INSTANCE;
-	
+		
 	private Connector() {
 	}
 	
-	public static synchronized FTPClient getInstance() {
-		if (INSTANCE == null) {
-			INSTANCE = new FTPClient();
-		}
-		return INSTANCE;
+	private static class ConnectionHolder {
+		private static final FTPClient CONNECTION = new FTPClient();
+	}
+	
+	public static FTPClient getInstance() {
+		
+		return ConnectionHolder.CONNECTION;
 	}
 	
 }
