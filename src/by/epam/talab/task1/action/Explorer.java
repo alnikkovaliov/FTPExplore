40ce7baf6ea6c73aa03dcef034f39866bc693ebe
Aliaksandr Kavaliou <Aliaksandr_Kavaliou@epam.com>
2015-02-06 14:17:30 +0300

    add new Exceptions to the throws section

--- src/by/epam/talab/task1/action/Explorer.java
+++ src/by/epam/talab/task1/action/Explorer.java
@@ -1,10 +1,15 @@
 package by.epam.talab.task1.action;
 
+
 import java.io.IOException;
 
+import it.sauronsoftware.ftp4j.FTPAbortedException;
 import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
 import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPFile;
 import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+import it.sauronsoftware.ftp4j.FTPListParseException;
 
 public final class Explorer {
 	
@@ -12,8 +17,19 @@ public final class Explorer {
 	}
 	
 	public static void changeDir(FTPClient client, String path) throws 
-	IllegalStateException, IOException, FTPIllegalReplyException, FTPException{
-		client.changeDirectory(path);
+	IllegalStateException, IOException, FTPIllegalReplyException, FTPException,
+	FTPDataTransferException, FTPAbortedException, FTPListParseException{
+		
+		FTPFile[] fileArray = client.list();
+		for (int i = 0; i < fileArray.length; i++) {
+			if (fileArray[i].getName() == path && 
+					fileArray[i].getType() == FTPFile.TYPE_DIRECTORY) {
+				client.changeDirectory(path);
+			} else {
+				throw new IllegalArgumentException("No such directory!");
+			}
+		}
+		
 				
 	}
 	
